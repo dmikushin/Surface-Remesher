53550ec8aa1048caf8eaa33dd86d3c2c4f1b1bd3
Dmitry Mikushin <dmitry@kernelgen.org>
2024-09-28 11:38:33 +0200

    Making the rendering script more user-friendly:
    * Adding command line description
    * Adding --choose-scene command line option, which will enforce choosing the scene and overwrite existing JSON with cached scene

--- examples/render.py
+++ examples/render.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 # Automatically render screenshots for the given meshes using pymeshlab and polyscope
 # Save the camera view, so that all subsequent screenshots use the same camera view
+import argparse
 import pymeshlab as ml
 import polyscope as ps
 import json
 import os
 import sys
 
-def render(filename):
+def render(filename, choose_scene=False):
     basename = filename[:filename.find('.')]
 
     ps.init()
+
+    # Do not use these functions, they break the good scaling and centering
+    # already applied by default!
     #ps.set_autocenter_structures(True)
     #ps.set_autoscale_structures(True)
+
     ps.set_up_dir("neg_y_up")
     ps.set_front_dir("neg_y_front")
     ps.set_ground_plane_mode("shadow_only")
@@ -29,11 +34,11 @@ def render(filename):
     ps_mesh.set_color([0.8, 0.8, 0.8])  # Set a default gray color
     ps.reset_camera_to_home_view()
 
-    # Look for camera parameters JSON and use it, if it is available;
+    # Look for camera parameters JSON and use it, if it is available and not to be ignored;
     # Otherwise, open GUI so that the user could choose the best camera view
     camera_json = f'{basename}.json'
-    if not os.path.isfile(camera_json):
-        print("No camera view is defined, please create it visually now")
+    if not os.path.isfile(camera_json) or choose_scene:
+        print("Please choose the camera view in the opened window and close it once finished")
 
         # View the point cloud and mesh we just registered in the 3D UI
         ps.show()
@@ -55,4 +60,13 @@ def render(filename):
     ps.remove_all_structures()
     ms.clear()
 
-render(sys.argv[1])
+def main():
+    parser = argparse.ArgumentParser(description='Automatically render screenshots for the given meshes using pymeshlab and polyscope. Save the camera view, so that all subsequent screenshots use the same camera view.')
+    parser.add_argument('filename', type=str, help='The filename of the mesh to render.')
+    parser.add_argument('--choose-scene', action='store_true', help='Enforce choosing the scene and overwrite the JSON with cached scene.')
+    args = parser.parse_args()
+
+    render(args.filename, args.choose_scene)
+
+if __name__ == "__main__":
+    main()
